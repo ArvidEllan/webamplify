a0fad047e1dec089f416eab1f1f595e35ebb77de
“ArvidEllan” <ellanwambugu6@gmail.com>
2023-08-31 11:18:51 +0300

    update authentication in auth

--- src/router/index.js
+++ src/router/index.js
@@ -35,4 +35,35 @@ const router = createRouter({
   routes,
 })
 
-export default router
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+// Adding method to chech if user is authenticated before moving navigating to the requested page
+router.beforeEach(async (to, from, next) => {
+  // Check if the route requires authentication by examining requiresAuth
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    try {
+      // Call the Amplify Auth method to check if the user is authenticated.
+      const user = await Auth.currentAuthenticatedUser();      
+      if (!user) {
+        // Not authenticated, so redirect to signin page.
+        next("/signin");
+      } 
+      else {
+        // They are authenticated, so we can continue.
+        next();
+      }
+    } catch (err) {
+      // Error, so redirect to signin page.
+      next("/signin");
+    }
+  } else {
+    // Route does not require authentication, so we can continue.
+    next();
+  }
+});
+// End of new method
+export default router;
+
